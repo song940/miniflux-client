064125770fd281906b9b5e452d9085df89733c68
Lsong <song940@gmail.com>
2022-09-27 16:23:05 +0800

    update

--- index.js
+++ index.js
@@ -32,8 +32,18 @@ export class Miniflux {
   post(path, payload) {
     return this.request('POST', path, payload);
   }
-  me() {
-    return this.getJSON('/v1/me');
+  async postJSON(path, payload) {
+    const response = await this.post(path, payload);
+    return response.json();
+  }
+  /**
+   * Discover Subscriptions
+   * https://miniflux.app/docs/api.html#endpoint-discover
+   * @param {*} url 
+   * @returns 
+   */
+  async discover(url) {
+    return this.postJSON('/v1/discover', { url });
   }
   /**
    * Get Feeds
@@ -43,12 +53,20 @@ export class Miniflux {
     return this.getJSON('/v1/feeds');
   }
   /**
+   * Get Category Feeds
+   * @docs https://miniflux.app/docs/api.html#endpoint-get-category-feeds
+   */
+  async get_category_feeds(category_id) {
+    return this.getJSON(`/v1/categories/${category_id}/feeds`);
+  }
+
+  /**
    * Get Feed
    * @docs https://miniflux.app/docs/api.html#endpoint-get-feed
    * @param {*} id 
    * @returns 
    */
-  feed(id) {
+  get_feed(id) {
     return this.getJSON(`/v1/feeds/${id}`);
   }
   /**
@@ -56,7 +74,7 @@ export class Miniflux {
    * @returns 
    */
   get_feed_icon(id) {
-    return this.get(`/v1/feeds/${id}/icon`);
+    return this.getJSON(`/v1/feeds/${id}/icon`);
   }
   /**
    * Get Category Feeds
@@ -65,7 +83,7 @@ export class Miniflux {
    * @returns 
    */
   get_feeds_by_category(id) {
-    return this.get(`/v1/categories/${id}/feeds`);
+    return this.getJSON(`/v1/categories/${id}/feeds`);
   }
   /**
    * Create Feed
@@ -75,7 +93,7 @@ export class Miniflux {
    * @returns 
    */
   create_feed(feed_url, category_id) {
-    return this.post(`/v1/feeds`, { feed_url, category_id });
+    return this.postJSON(`/v1/feeds`, { feed_url, category_id });
   }
   /**
    * Update Feed
@@ -104,15 +122,204 @@ export class Miniflux {
   refresh_all_feeds() {
     return this.request('PUT', '/v1/feeds/refresh');
   }
+  /**
+   * Refresh Feed
+   * @docs https://miniflux.app/docs/api.html#endpoint-refresh-feed
+   * @param {*} id 
+   * @returns 
+   */
   remove_feed(id) {
     return this.request('DELETE', `/v1/feeds/${id}`);
   }
+  /**
+   * Get Feed Entry
+   * @docs https://miniflux.app/docs/api.html#endpoint-get-feed-entry
+   * @returns 
+   */
+  get_feed_entry(feed_id, entry_id) {
+    return this.getJSON(`/v1/feeds/${feed_id}/entries/${entry_id}`);
+  }
+  /**
+   * Fetch original article
+   * @docs https://miniflux.app/docs/api.html#endpoint-fetch-content
+   * @param {*} entry_id 
+   */
+  fetch_entry_content(entry_id) {
+    return this.getJSON(`/entries/${entry_id}/fetch-content`);
+  }
+  /**
+   * Get Category Entries
+   * @docs https://miniflux.app/docs/api.html#endpoint-get-category-entries
+   * @param {*} category_id 
+   * @param {*} param1 
+   * @returns 
+   */
+  get_category_entries(category_id, { limit = 1, order = 'id', direction = 'asc' } = {}) {
+    return this.getJSON(`/v1/categories/${category_id}/entries?limit=${limit}&order=${order}&direction=${direction}`);
+  }
+  /**
+   * Get Feed Entries
+   * @docs https://miniflux.app/docs/api.html#endpoint-get-feed-entries
+   */
+  get_feed_entries(feed_id, { limit = 1, order = 'id', direction = 'asc' } = {}) {
+    return this.getJSON(`/v1/feeds/${feed_id}/entries?limit=${limit}&order=${order}&direction=${direction}`);
+  }
+  /**
+   * Get Entries 
+   * @docs https://miniflux.app/docs/api.html#endpoint-get-entries
+   * @param {*}
+   */
+  get_entries(status = 'unread', direction = 'desc') {
+    return this.getJSON(`/v1/entries?status=${status}&direction=${direction}`);
+  }
+  /**
+   * Update Entries
+   * @docs https://miniflux.app/docs/api.html#endpoint-update-entries
+   */
+  async update_entries(entry_ids, status = 'unread') {
+    const response = await this.request('PUT', '/v1/entries', { entry_ids, status });
+    return response.status === 204;
+  }
+  /**
+   * Toggle Entry Bookmark
+   * @docs https://miniflux.app/docs/api.html#endpoint-toggle-
+   */
+  async toggle_entry_bookmark(entry_id) {
+    const response = await this.request('PUT', `/v1/entries/${entry_id}/bookmark`);
+    return response.status === 204;
+  }
+  /**
+   * Create Category
+   * @docs https://miniflux.app/docs/api.html#endpoint-create-category
+   * @param {*} title 
+   * @returns 
+   */
+  async create_category(title) {
+    return this.postJSON('/v1/categories', { title });
+  }
+  /**
+   * Update Category
+   * @docs https://miniflux.app/docs/api.html#endpoint-update-category
+   * @param {*} category_id 
+   * @returns 
+   */
+  async update_category(category_id) {
+    const response = await this.request('PUT', `/v1/categories/${category_id}`);
+    return response.json();
+  }
+  /**
+   * Delete Category
+   * @docs https://miniflux.app/docs/api.html#endpoint-delete-category
+   */
+  async delete_category(category_id) {
+    const response = await this.request('DELETE', `/v1/categories/${category_id}`);
+    return response.status === 204;
+  }
+  /**
+   * Mark Category Entries as Read
+   * @docs https://miniflux.app/docs/api.html#endpoint-mark-category-entries-as-read
+   * @param {*} category_id 
+   * @returns 
+   */
+  async mark_category_entries_as_read(category_id) {
+    const response = await this.request('PUT', `/v1/categories/${category_id}/mark-all-as-read`);
+    return response.status === 204;
+  }
+  async export() {
+    const response = await this.request('GET', '/v1/export');
+    return response.text();
+  }
+  async import(opml) {
+    const response = await this.request('/v1/import', opml);
+    return response.status === 201;
+  }
+  /**
+   * Create User
+   * @docs https://miniflux.app/docs/api.html#endpoint-create-user
+   * @param {*} user 
+   * @returns 
+   */
+  async create_user(user) {
+    return this.postJSON('/v1/users', user);
+  }
+  /**
+   * Update User
+   * @param {*} user_id 
+   * @param {*} user 
+   * @returns 
+   */
+  async update_user(user_id, user) {
+    const response = await this.request('PUT', `/v1/users/${user_id}`, user);
+    return response.json();
+  }
+  /**
+   * Get Current User
+   * @returns 
+   */
+  me() {
+    return this.getJSON('/v1/me');
+  }
+  /**
+   * Get User
+   * @docs https://miniflux.app/docs/api.html#endpoint-get-user
+   * @param {*} user_id 
+   * @returns 
+   */
+  get_user(user_id) {
+    return this.getJSON(`/v1/users/${user_id}`);
+  }
+  /**
+   * Get Users
+   * @docs https://miniflux.app/docs/api.html#endpoint-get-users
+   * @returns 
+   */
+  get_users() {
+    return this.getJSON('/v1/users');
+  }
+  /**
+   * Delete User
+   * @docs https://miniflux.app/docs/api.html#endpoint-delete-user
+   * @param {*} user_id 
+   * @returns 
+   */
+  async delete_user(user_id) {
+    const response = await this.request('DELETE', `/v1/users/${user_id}`);
+    return response.status === 204;
+  }
+  /**
+   * Mark User Entries as Read
+   * @docs https://miniflux.app/docs/api.html#endpoint-mark-user-entries-as-read
+   * @param {*} user_id 
+   * @returns 
+   */
+  async mark_user_entries_as_read(user_id) {
+    const response = await this.request('PUT', `/v1/users/${user_id}/mark-all-as-read`);
+    return response.status === 204;
+  }
+  /**
+   * Fetch Read/Unread Counters
+   * @docs https://miniflux.app/docs/api.html#endpoint-counters
+   * @returns 
+   */
+  async fetch_counters() {
+    return this.getJSON('/v1/feeds/counters');
+  }
+  /**
+   * Healthcheck
+   * @docs https://miniflux.app/docs/api.html#endpoint-healthcheck
+   * @returns 
+   */
   healthcheck() {
     return this.get('/v1/healthcheck');
   }
+  /**
+   * Version
+   * @docs https://miniflux.app/docs/api.html#endpoint-version
+   * @returns 
+   */
   async version() {
     const response = await this.get('/version');
-    if(response.status !== 200) throw new Error();
+    if (response.status !== 200) throw new Error();
     return response.text();
   }
 }
--- test/index.js
+++ test/index.js
@@ -24,8 +24,45 @@ test('miniflux#me', async () => {
   assert.equal(me.username, 'song940');
 });
 
+test('mimniflux#discover', async () => {
+  const feeds = await miniflux.discover('https://github.com/song940');
+  assert.ok(feeds);
+  assert.ok(Array.isArray(feeds));
+  assert.equal(feeds[0].type, 'atom');
+  assert.equal(feeds[0].url, 'https://github.com/feed/');
+});
+
 test('mimniflux#feeds', async () => {
   const feeds = await miniflux.feeds();
   assert.ok(feeds);
   assert.ok(Array.isArray(feeds));
+});
+
+test('mimniflux#get_category_feeds', async () => {
+  const feeds = await miniflux.get_category_feeds(1);
+  assert.ok(feeds);
+  assert.ok(Array.isArray(feeds));
+});
+
+test('mimniflux#get_feed', async () => {
+  const feed = await miniflux.get_feed(1);
+  assert.ok(feed);
+});
+
+
+test('mimniflux#get_feed_icon', async () => {
+  const icon = await miniflux.get_feed_icon(2);
+  assert.ok(icon);
+  assert.equal(icon.mime_type, 'image/png');
+});
+
+test('mimniflux#get_feeds_by_category', async () => {
+  const feeds = await miniflux.get_feeds_by_category(1);
+  assert.ok(feeds);
+});
+
+test('mimniflux#get_entries', async () => {
+  const entries = await miniflux.get_entries(1);
+  console.log(entries);
+  assert.ok(entries);
 });
